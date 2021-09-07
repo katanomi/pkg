9d0c571f84f2cbf83d2a84352d95e4fe2fd5d684
Boya Xiao <34267152+XiaoBoya@users.noreply.github.com>
2021-09-07 13:51:32 +0800

    feat:add commit comment and status (#64)

---
+++ plugin/client/gitcommitcomment.go
@@ -0,0 +1,84 @@
+/*
+Copyright 2021 The Katanomi Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package client
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+
+	metav1alpha1 "github.com/katanomi/pkg/apis/meta/v1alpha1"
+	duckv1 "knative.dev/pkg/apis/duck/v1"
+)
+
+type ClientGitCommitComment interface {
+	List(ctx context.Context, baseURL *duckv1.Addressable, option metav1alpha1.GitCommitOption, options ...OptionFunc) (*metav1alpha1.GitCommitCommentList, error)
+	Create(ctx context.Context, baseURL *duckv1.Addressable, payload metav1alpha1.CreateCommitCommentPayload, options ...OptionFunc) (*metav1alpha1.GitCommitComment, error)
+}
+
+type gitCommitComment struct {
+	client Client
+	meta   Meta
+	secret corev1.Secret
+}
+
+func newGitCommitComment(client Client, meta Meta, secret corev1.Secret) ClientGitCommitComment {
+	return &gitCommitComment{
+		client: client,
+		meta:   meta,
+		secret: secret,
+	}
+}
+
+func (g *gitCommitComment) List(ctx context.Context, baseURL *duckv1.Addressable, option metav1alpha1.GitCommitOption, options ...OptionFunc) (*metav1alpha1.GitCommitCommentList, error) {
+	commitCommentList := &metav1alpha1.GitCommitCommentList{}
+	options = append(options, MetaOpts(g.meta), SecretOpts(g.secret), ResultOpts(commitCommentList))
+	if option.Repository == "" {
+		return nil, errors.New("repo is empty string")
+	} else if option.Project == "" {
+		return nil, errors.New("project is empty string")
+	} else if option.SHA == nil {
+		return nil, errors.New("unknown sha for commit")
+	}
+	sha := *option.SHA
+	uri := fmt.Sprintf("/projects/%s/coderepositories/%s/commit/%s/comments", option.Project, option.Repository, sha)
+	if err := g.client.Get(ctx, baseURL, uri, options...); err != nil {
+		return nil, err
+	}
+	return commitCommentList, nil
+}
+
+func (g *gitCommitComment) Create(ctx context.Context, baseURL *duckv1.Addressable, payload metav1alpha1.CreateCommitCommentPayload, options ...OptionFunc) (*metav1alpha1.GitCommitComment, error) {
+	commentInfo := &metav1alpha1.GitCommitComment{}
+	options = append(options, MetaOpts(g.meta), SecretOpts(g.secret), BodyOpts(payload.CreateCommitCommentParam), ResultOpts(commentInfo))
+	if payload.Repository == "" {
+		return nil, errors.New("repo is empty string")
+	} else if payload.Project == "" {
+		return nil, errors.New("project is empty string")
+	} else if payload.SHA == nil {
+		return nil, errors.New("unknown sha for commit")
+	}
+	sha := *payload.SHA
+	uri := fmt.Sprintf("/projects/%s/coderepositories/%s/commit/%s/comments", payload.Project, payload.Repository, sha)
+	if err := g.client.Post(ctx, baseURL, uri, options...); err != nil {
+		return nil, err
+	}
+
+	return commentInfo, nil
+}
---
+++ plugin/route/gitcommitcomment.go
@@ -0,0 +1,120 @@
+/*
+Copyright 2021 The Katanomi Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package route
+
+import (
+	"net/http"
+
+	kerrors "github.com/katanomi/pkg/errors"
+
+	restfulspec "github.com/emicklei/go-restful-openapi/v2"
+	"github.com/emicklei/go-restful/v3"
+	metav1alpha1 "github.com/katanomi/pkg/apis/meta/v1alpha1"
+	"github.com/katanomi/pkg/plugin/client"
+)
+
+type gitCommitCommentLister struct {
+	impl client.GitCommitCommentLister
+	tags []string
+}
+
+// NewGitCommitCommentLister get git Commit comment route with plugin client
+func NewGitCommitCommentLister(impl client.GitCommitCommentLister) Route {
+	return &gitCommitCommentLister{
+		tags: []string{"git", "repositories", "commit comment"},
+		impl: impl,
+	}
+}
+
+// Register route
+func (a *gitCommitCommentLister) Register(ws *restful.WebService) {
+	repositoryParam := ws.PathParameter("repository", "commit belong to repository")
+	shaParam := ws.PathParameter("sha", "commit sha")
+	projectParam := ws.PathParameter("project", "repository belong to project")
+	ws.Route(
+		ws.GET("/projects/{project}/coderepositories/{repository}/commit/{sha}/comments").To(a.ListGitCommitComment).
+			Doc("ListGitCommitComment").Param(projectParam).Param(repositoryParam).Param(shaParam).
+			Metadata(restfulspec.KeyOpenAPITags, a.tags).
+			Returns(http.StatusOK, "OK", metav1alpha1.GitCommitCommentList{}),
+	)
+}
+
+// ListGitCommitComment List commit info
+func (a *gitCommitCommentLister) ListGitCommitComment(request *restful.Request, response *restful.Response) {
+	option := GetListOptionsFromRequest(request)
+	sha := request.PathParameter("sha")
+	repo := request.PathParameter("repository")
+	project := request.PathParameter("project")
+	commitOption := metav1alpha1.GitCommitOption{
+		GitRepo:            metav1alpha1.GitRepo{Repository: repo, Project: project},
+		GitCommitBasicInfo: metav1alpha1.GitCommitBasicInfo{SHA: &sha},
+	}
+	commitList, err := a.impl.ListGitCommitComment(request.Request.Context(), commitOption, option)
+	if err != nil {
+		kerrors.HandleError(request, response, err)
+		return
+	}
+	response.WriteHeaderAndEntity(http.StatusOK, commitList)
+}
+
+type gitCommitCommentCreator struct {
+	impl client.GitCommitCommentCreator
+	tags []string
+}
+
+// NewGitCommitCommentCreator create git Commit comment route with plugin client
+func NewGitCommitCommentCreator(impl client.GitCommitCommentCreator) Route {
+	return &gitCommitCommentCreator{
+		tags: []string{"git", "repositories", "commit comment"},
+		impl: impl,
+	}
+}
+
+// Register route
+func (a *gitCommitCommentCreator) Register(ws *restful.WebService) {
+	repositoryParam := ws.PathParameter("repository", "commit belong to repository")
+	shaParam := ws.PathParameter("sha", "commit sha")
+	projectParam := ws.PathParameter("project", "repository belong to project")
+	ws.Route(
+		ws.POST("/projects/{project}/coderepositories/{repository}/commit/{sha}/comments").To(a.CreateGitCommitComment).
+			Doc("CreateGitCommitComment").Param(projectParam).Param(repositoryParam).Param(shaParam).
+			Metadata(restfulspec.KeyOpenAPITags, a.tags).
+			Returns(http.StatusOK, "OK", metav1alpha1.GitCommitComment{}),
+	)
+}
+
+// CreateGitCommitComment create commit info
+func (a *gitCommitCommentCreator) CreateGitCommitComment(request *restful.Request, response *restful.Response) {
+	sha := request.PathParameter("sha")
+	repo := request.PathParameter("repository")
+	project := request.PathParameter("project")
+	var params metav1alpha1.CreateCommitCommentParam
+	if err := request.ReadEntity(&params); err != nil {
+		kerrors.HandleError(request, response, err)
+		return
+	}
+	commitComment, err := a.impl.CreateGitCommitComment(request.Request.Context(), metav1alpha1.CreateCommitCommentPayload{
+		GitRepo:                  metav1alpha1.GitRepo{Project: project, Repository: repo},
+		GitCommitBasicInfo:       metav1alpha1.GitCommitBasicInfo{SHA: &sha},
+		CreateCommitCommentParam: params,
+	})
+	if err != nil {
+		kerrors.HandleError(request, response, err)
+		return
+	}
+	response.WriteHeaderAndEntity(http.StatusOK, commitComment)
+}
