68a08eebe06960d6f82d189f2642be4be6a89b05
Daniel Filipe Bruehmueller Morinigo <danielfbm@gmail.com>
2021-10-30 11:38:16 +0800

    chore: Add unit tests for triggeredBy and ObjectCondition in apis/meta/v1alpha1

--- apis/meta/v1alpha1/objectcondition_types_test.go
+++ apis/meta/v1alpha1/objectcondition_types_test.go
@@ -19,10 +19,17 @@ package v1alpha1
 import (
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	ktesting "github.com/katanomi/pkg/testing"
+	mockmetav1alpha1 "github.com/katanomi/pkg/testing/mock/github.com/katanomi/pkg/apis/meta/v1alpha1"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"knative.dev/pkg/apis"
 )
+
+//go:generate ../../../bin/mockgen -package=apis -destination=../../../testing/mock/github.com/katanomi/pkg/apis/meta/v1alpha1/top_level_condition_object.go  github.com/katanomi/pkg/apis/meta/v1alpha1 TopLevelConditionObject
 
 func TestObjectConditions(t *testing.T) {
 	g := NewGomegaWithT(t)
@@ -50,6 +57,39 @@ func TestObjectConditions(t *testing.T) {
 	g.Expect(abcobjcs).To(HaveLen(2))
 	diff = cmp.Diff(abcobjcs[1], abcPod)
 	g.Expect(diff).To(BeEmpty())
+}
+
+func TestFromTopLevelConditionObject(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	abcPod := &ObjectCondition{}
+	g.Expect(ktesting.LoadYAML("testdata/objectcondition-pod-abc.yaml", abcPod)).To(Succeed())
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	condition := &apis.Condition{
+		Type:    apis.ConditionReady,
+		Status:  corev1.ConditionTrue,
+		Reason:  "Reason",
+		Message: "message",
+	}
+
+	topLevelCondition := mockmetav1alpha1.NewMockTopLevelConditionObject(ctrl)
+	topLevelCondition.EXPECT().GetTopLevelCondition().Return(condition).Times(2)
+	topLevelCondition.EXPECT().GetAnnotations().Return(map[string]string{"abc": "def"}).Times(1)
+	topLevelCondition.EXPECT().GetName().Return("some-name")
+	topLevelCondition.EXPECT().GetNamespace().Return("default")
+	uid := types.UID("1234")
+	topLevelCondition.EXPECT().GetUID().Return(uid)
+
+	abcPod.FromTopLevelConditionObject(topLevelCondition)
+
+	g.Expect(abcPod.Condition).To(Equal(*condition))
+	g.Expect(abcPod.Name).To(Equal("some-name"))
+	g.Expect(abcPod.Namespace).To(Equal("default"))
+	g.Expect(abcPod.UID).To(Equal(uid))
+	g.Expect(abcPod.Annotations).To(Equal(map[string]string{"abc": "def"}))
 }
 
 func TestDeepCopy(t *testing.T) {
