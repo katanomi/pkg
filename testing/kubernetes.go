637775728c516119a6ba8a5608fffb673be44b05
l-qing <9499086+l-qing@users.noreply.github.com>
2022-01-05 15:18:05 +0800

    feat: loaded resources support list of fake client (#150)
    This logic can be removed after upgrading to controller-runtime v0.10.1
    Ref: https://github.com/kubernetes-sigs/controller-runtime/pull/1662
    
    Co-authored-by: qingliu <qingliu@alauda.io>

--- testing/kubernetes.go
+++ testing/kubernetes.go
@@ -56,17 +56,33 @@ func LoadKubeResourcesAsUnstructured(file string) (objs []unstructured.Unstructu
 }
 
 // LoadKubeResources loading kubernetes resources
-func LoadKubeResources(file string, clt client.Client) (err error) {
+func LoadKubeResources(file string, clt client.Client, converts ...ConvertRuntimeObjctToClientObjectFunc) (err error) {
+	errs := []error{}
 	objs, err := LoadKubeResourcesAsUnstructured(file)
 	if err != nil {
 		return
 	}
+OUTER:
 	for _, obj := range objs {
+		runtimeObj, err := convertFromUnstructuredIfNecessary(clt.Scheme(), &obj)
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+		for _, convert := range converts {
+			if clientObj, err := convert(runtimeObj); err == nil {
+				if err = clt.Create(context.Background(), clientObj); err != nil {
+					errs = append(errs, err)
+				}
+				continue OUTER
+			}
+		}
 		if err = clt.Create(context.Background(), &obj); err != nil {
-			return
+			errs = append(errs, err)
+			continue
 		}
 	}
-	return
+	return errors.NewAggregate(errs)
 }
 
 // UnstructedToTyped converts an unstructured object into a object
@@ -134,4 +150,36 @@ func LoadResourceFromFile(scheme *runtime.Scheme, path string) (obj runtime.Obje
 		return nil, err
 	}
 	return objs[0], nil
+}
+
+type ConvertRuntimeObjctToClientObjectFunc func(runtime.Object) (client.Object, error)
+
+// This logic can be removed after upgrading to controller-runtime v0.10.1
+// Copy from: https://github.com/kubernetes-sigs/controller-runtime/pull/1662/files
+
+// convertFromUnstructuredIfNecessary will convert *unstructured.Unstructured for a GVK that is recocnized
+// by the schema into the whatever the schema produces with New() for said GVK.
+// This is required because the tracker unconditionally saves on manipulations, but it's List() implementation
+// tries to assign whatever it finds into a ListType it gets from schema.New() - Thus we have to ensure
+// we save as the very same type, otherwise subsequent List requests will fail.
+func convertFromUnstructuredIfNecessary(s *runtime.Scheme, o runtime.Object) (runtime.Object, error) {
+	u, isUnstructured := o.(*unstructured.Unstructured)
+	if !isUnstructured || !s.Recognizes(u.GroupVersionKind()) {
+		return o, nil
+	}
+
+	typed, err := s.New(u.GroupVersionKind())
+	if err != nil {
+		return nil, fmt.Errorf("scheme recognizes %s but failed to produce an object for it: %w", u.GroupVersionKind().String(), err)
+	}
+
+	unstructuredSerialized, err := json.Marshal(u)
+	if err != nil {
+		return nil, fmt.Errorf("failed to serialize %T: %w", unstructuredSerialized, err)
+	}
+	if err := json.Unmarshal(unstructuredSerialized, typed); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal the content of %T into %T: %w", u, typed, err)
+	}
+
+	return typed, nil
 }
