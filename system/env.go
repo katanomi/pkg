f9f53b8d5bb547410b3cc5935c7c428730a8073a
kychen <kychen@alauda.io>
2022-01-24 12:27:52 +0800

    feat: add read cluster name from env.

---
+++ system/env.go
@@ -0,0 +1,33 @@
+/*
+Copyright 2022 The Katanomi Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package env
+
+import (
+	"os"
+)
+
+const (
+	// ClusterNameEnvKey is the environment variable that specifies the system cluster name.
+	// ClusterName can be attached to the created resource. When there are multiple clusters, the resource can be found by the api by the cluster name.
+	ClusterNameEnvKey = "SYSTEM_CLUSTER_NAME"
+)
+
+// ClusterName returns the cluster name that describes the cluster name the instance runs on.
+// You need to specify the relevant environment variables through deployment when deploying.
+func ClusterName() string {
+	return os.Getenv(ClusterNameEnvKey)
+}
