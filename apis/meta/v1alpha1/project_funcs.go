52a46bff12dec2a0797a54e8a039df13297dcc4a
Yukun Wang <airycanon@airycanon.me>
2022-03-14 22:03:05 +0800

    add project list funcs (#179)

--- apis/meta/v1alpha1/project_funcs.go
+++ apis/meta/v1alpha1/project_funcs.go
@@ -16,7 +16,11 @@ limitations under the License.
 
 package v1alpha1
 
-import v1 "k8s.io/api/core/v1"
+import (
+	"sort"
+
+	v1 "k8s.io/api/core/v1"
+)
 
 func (p *Project) AddNamespaceRef(refs ...v1.ObjectReference) {
 	for _, item := range refs {
@@ -32,4 +36,63 @@ func (p *Project) AddNamespaceRef(refs ...v1.ObjectReference) {
 			p.Spec.NamespaceRefs = append(p.Spec.NamespaceRefs, item)
 		}
 	}
+}
+
+const (
+	FirstPage       = 1
+	DefaultPageSize = 20
+)
+
+// Paginate return a pagination subset of project list with specific page and page size
+func (p *ProjectList) Paginate(page int, pageSize int) *ProjectList {
+	if page < FirstPage {
+		page = FirstPage
+	}
+
+	if pageSize < FirstPage {
+		pageSize = DefaultPageSize
+	}
+
+	length := len(p.Items)
+	skip := (page - 1) * pageSize
+
+	if skip > length {
+		skip = length
+	}
+
+	end := skip + pageSize
+	if end > length {
+		end = length
+	}
+
+	newList := &ProjectList{}
+	newList.Items = p.Items[skip:end]
+	newList.ListMeta.TotalItems = length
+
+	return newList
+}
+
+// Sort project list by name
+func (p *ProjectList) Sort() *ProjectList {
+	sort.Slice(p.Items, func(i, j int) bool {
+		return p.Items[i].Name < p.Items[j].Name
+	})
+
+	return p
+}
+
+// Filter takes a closure that returns true or false, if true, the project should be present
+func (p *ProjectList) Filter(filter func(project Project) bool) *ProjectList {
+	if filter == nil {
+		return p
+	}
+
+	newList := &ProjectList{}
+	for _, project := range p.Items {
+		if filter(project) {
+			newList.Items = append(newList.Items, project)
+		}
+	}
+
+	return newList
 }
