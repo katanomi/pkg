85677c1c25d46dad99b1f55732070b613c39bf99
NigelZHANG <1135238080@qq.com>
2022-06-10 15:18:47 +0800

    add git resource attribute (#218)
    Co-authored-by: jjzhang <jjzhang@alauda.io>

---
+++ apis/meta/v1alpha1/code_quality_types_test.go
@@ -0,0 +1,39 @@
+/*
+Copyright 2021 The Katanomi Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+	authv1 "k8s.io/api/authorization/v1"
+)
+
+func TestCodeQualityResourceAttributes(t *testing.T) {
+	attr := authv1.ResourceAttributes{
+		Group:    "meta.katanomi.dev",
+		Version:  "v1alpha1",
+		Resource: "codequalities",
+		Verb:     "list",
+	}
+
+	t.Run("list codequalities", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+		codeAttr := CodeQualityResourceAttributes("list")
+		g.Expect(attr, codeAttr)
+	})
+}
