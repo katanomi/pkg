ea1a7ce65b5d8694e6938eb06eb5d4934809158b
nanjingfm <765390765@qq.com>
2022-10-11 15:46:04 +0800

    chore: add helper function for `ListOption` (#298)
    Co-authored-by: fm <mingfu@alauda.cn>

--- apis/meta/v1alpha1/listmeta_types_test.go
+++ apis/meta/v1alpha1/listmeta_types_test.go
@@ -75,3 +75,132 @@ func TestListOptions_DefaultPager(t *testing.T) {
 	Equal(opt2.Page).Match(10)
 	Equal(opt2.ItemsPerPage).Match(30)
 }
+
+func TestListOptions_MatchSearchValue(t *testing.T) {
+	g := NewGomegaWithT(t)
+	tests := []struct {
+		name   string
+		Search map[string][]string
+		want   bool
+	}{
+		{
+			Search: map[string][]string{
+				SearchValueKey: {"test", "test2"},
+			},
+			name: "",
+			want: false,
+		},
+		{
+			Search: map[string][]string{
+				SearchValueKey: {"test", "test2"},
+			},
+			name: "test4",
+			want: true,
+		},
+		{
+			Search: map[string][]string{
+				SearchValueKey: {"test", "test2"},
+			},
+			name: "abc",
+			want: false,
+		},
+		{
+			Search: map[string][]string{
+				SearchValueKey: {"test", "test2"},
+			},
+			name: "test2",
+			want: true,
+		},
+		{
+			Search: map[string][]string{
+				SearchValueKey: {"test", "test2"},
+			},
+			name: "test",
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			opt := &ListOptions{
+				Search: tt.Search,
+			}
+			g.Expect(opt.MatchSearchValue(tt.name)).To(Equal(tt.want))
+		})
+	}
+}
+
+func TestListOptions_MatchSubResource(t *testing.T) {
+	g := NewGomegaWithT(t)
+	tests := []struct {
+		name         string
+		subResources []string
+		want         bool
+	}{
+		{
+			subResources: []string{"test", "test2"},
+			name:         "",
+			want:         false,
+		},
+		{
+			subResources: []string{"test", "test2"},
+			name:         "abc",
+			want:         false,
+		},
+		{
+			subResources: []string{"test", "test2"},
+			name:         "test2",
+			want:         true,
+		},
+		{
+			subResources: []string{"test", "test2"},
+			name:         "test",
+			want:         true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			opt := &ListOptions{
+				SubResources: tt.subResources,
+			}
+			g.Expect(opt.MatchSubResource(tt.name)).To(Equal(tt.want))
+		})
+	}
+}
+
+func Test_getSearchValue(t *testing.T) {
+	g := NewGomegaWithT(t)
+	tests := []struct {
+		option ListOptions
+		want   string
+	}{
+		{
+			option: ListOptions{
+				Search: map[string][]string{
+					SearchValueKey: {"test", "test2"},
+				},
+			},
+			want: "test",
+		},
+		{
+			option: ListOptions{
+				Search: map[string][]string{
+					"name": {"test", "test2"},
+				},
+			},
+			want: "test",
+		},
+		{
+			option: ListOptions{},
+			want:   "",
+		},
+		{
+			option: ListOptions{
+				Search: nil,
+			},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		g.Expect(GetSearchValue(tt.option)).To(Equal(tt.want))
+	}
+}
