8deca94cb3d956c7d9608226cdb364bcf36cff37
jzli <jzli@alauda.io>
2022-12-14 09:23:27 +0800

    fix: env-option-validation-msg  1. Add validation option funcs to provides builtin and customize validation rules.  2. Simply intersect func.

--- command/args/key_value.go
+++ command/args/key_value.go
@@ -18,23 +18,79 @@ package args
 
 import (
 	"context"
+	"fmt"
 	// "fmt"
 	"strings"
+
+	"github.com/katanomi/pkg/common"
+	"k8s.io/apimachinery/pkg/util/errors"
 )
+
+// ValuesValidateOption provides extensive func to set validation rules of GetKeyValues
+type ValuesValidateOption func(values []string) error
+
+// ValuesValidationOptRequired is the builtin option for validating values are not empty
+var ValuesValidationOptRequired ValuesValidateOption = func(values []string) error {
+	if len(values) == 0 {
+		return fmt.Errorf("empty values")
+	}
+	return nil
+}
+
+// ValuesValidationOptDuplicatedKeys is the builtin option for validating values are unique key=value format
+var ValuesValidationOptDuplicatedKeys ValuesValidateOption = func(pairs []string) error {
+	var duplicatedKeys, invalidPairs []string
+	keyCount := map[string]int{}
+
+	for _, pair := range pairs {
+		split := strings.SplitN(pair, "=", 2)
+		if len(split) != 2 && !common.Contains(invalidPairs, pair) {
+			invalidPairs = append(invalidPairs, pair)
+			continue
+		}
+		pairKey := split[0]
+		keyCount[pairKey]++
+		if keyCount[pairKey] == 2 {
+			duplicatedKeys = append(duplicatedKeys, pairKey)
+		}
+	}
+
+	if len(duplicatedKeys) == 0 && len(invalidPairs) == 0 {
+		return nil
+	}
+
+	var duplicatedKeysErrMsg, invalidPairsErrMsg string
+	if len(duplicatedKeys) > 0 {
+		duplicatedKeysErrMsg = fmt.Sprintf("duplicated env keys: %s", duplicatedKeys)
+	}
+
+	if len(invalidPairs) > 0 {
+		invalidPairsErrMsg = fmt.Sprintf("invalid env items: %s", invalidPairs)
+	}
+	return fmt.Errorf("invalid env-flags: %s", strings.Trim(strings.Join([]string{duplicatedKeysErrMsg,
+		invalidPairsErrMsg},
+		","), ","))
+}
 
 // GetKeyValues returns the set of keys and values for one flag in a set of given arguments
 // args should be all the arguments to be interpreted. i.e --arg1 key1=value1 key2=value2 --arg2 key3=value3
 // flag should be the flag name to select keys and values from, i.e arg1 or arg2 in the example above
-func GetKeyValues(ctx context.Context, args []string, flag string) (keyValues map[string]string, ok bool) {
+func GetKeyValues(ctx context.Context, args []string, flag string,
+	opts ...ValuesValidateOption) (keyValues map[string]string,
+	err error) {
 	keyValues = map[string]string{}
-	var values []string
-	if values, ok = GetArrayValues(ctx, args, flag); ok {
-		for _, arg := range values {
-			split := strings.SplitN(arg, "=", 2)
-			if len(split) == 2 {
-				keyValues[split[0]] = split[1]
-			}
+	values, _ := GetArrayValues(ctx, args, flag)
+	for _, arg := range values {
+		split := strings.SplitN(arg, "=", 2)
+		if len(split) == 2 {
+			keyValues[split[0]] = split[1]
 		}
 	}
-	return
+	var errs []error
+	for _, opt := range opts {
+		if optErr := opt(values); optErr != nil {
+			errs = append(errs, optErr)
+		}
+	}
+	return keyValues, errors.NewAggregate(errs)
 }
--- command/args/key_value_test.go
+++ command/args/key_value_test.go
@@ -18,6 +18,8 @@ package args_test
 
 import (
 	"context"
+	"fmt"
+	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -31,7 +33,8 @@ var _ = Describe("GetKeyValues", func() {
 		cliArgs []string
 		flag    string
 		result  map[string]string
-		ok      bool
+		err     error
+		opts    []args.ValuesValidateOption
 	)
 	BeforeEach(func() {
 		ctx = context.Background()
@@ -39,35 +42,38 @@ var _ = Describe("GetKeyValues", func() {
 		flag = ""
 	})
 	JustBeforeEach(func() {
-		result, ok = args.GetKeyValues(ctx, cliArgs, flag)
+		result, err = args.GetKeyValues(ctx, cliArgs, flag, opts...)
 	})
 	When("args are nil", func() {
 		BeforeEach(func() {
 			cliArgs = nil
+			opts = []args.ValuesValidateOption{args.ValuesValidationOptRequired}
 		})
-		It("should return empty map with false ok", func() {
+		It("should return empty map with error", func() {
 			Expect(result).To(Equal(map[string]string{}))
-			Expect(ok).To(BeFalse())
+			Expect(err).Should(HaveOccurred())
 		})
 	})
 	When("args does not have the flag", func() {
 		BeforeEach(func() {
 			cliArgs = []string{"--flag1", "key=value", "--other-flag", "another-flag-key=123"}
 			flag = "non-existing-flag"
+			opts = []args.ValuesValidateOption{args.ValuesValidationOptRequired}
 		})
-		It("should return empty map with false ok", func() {
+		It("should return empty map with error", func() {
 			Expect(result).To(Equal(map[string]string{}))
-			Expect(ok).To(BeFalse())
+			Expect(err).To(HaveOccurred())
 		})
 	})
 	When("flag does not have items", func() {
 		BeforeEach(func() {
 			cliArgs = []string{"--flag1", "--other-flag", "another-flag-key=123"}
 			flag = "flag1"
+			opts = []args.ValuesValidateOption{args.ValuesValidationOptRequired}
 		})
-		It("should return empty map with false ok", func() {
+		It("should return empty map with error", func() {
 			Expect(result).To(Equal(map[string]string{}))
-			Expect(ok).To(BeTrue())
+			Expect(err).Should(HaveOccurred())
 		})
 	})
 	When("flag does not have key-value pairs", func() {
@@ -75,9 +81,9 @@ var _ = Describe("GetKeyValues", func() {
 			cliArgs = []string{"--flag1", "this-is-a-value", "this-is-another-value", "--other-flag", "another-flag-key=123"}
 			flag = "flag1"
 		})
-		It("should return empty map with true ok", func() {
+		It("should return empty map without error", func() {
 			Expect(result).To(Equal(map[string]string{}))
-			Expect(ok).To(BeTrue())
+			Expect(err).ShouldNot(HaveOccurred())
 		})
 	})
 	When("flag has key-value pairs", func() {
@@ -87,13 +93,98 @@ var _ = Describe("GetKeyValues", func() {
 				"--other-flag", "another-flag-key=123"}
 			flag = "flag1"
 		})
-		It("should return map with keys and values and true ok", func() {
+		It("should return map with keys and values and nil error", func() {
 			Expect(result).To(Equal(map[string]string{
 				"this-is-key":         "this-is-value",
 				"this-is-another-key": "this-is-another-value",
 				"key":                 "with=multiple=equalsigns",
 			}))
-			Expect(ok).To(BeTrue())
+			Expect(err).ShouldNot(HaveOccurred())
 		})
 	})
+
+	When("flag with failed validation option", func() {
+		BeforeEach(func() {
+			cliArgs = []string{
+				"--flag1", "this-is-key=this-is-value", "this-is-another-key=this-is-another-value", "key=with=multiple=equalsigns",
+				"--other-flag", "another-flag-key=123"}
+			flag = "flag1"
+			opts = []args.ValuesValidateOption{
+				func(values []string) error {
+					return fmt.Errorf("invalid validation")
+				},
+			}
+		})
+		It("should return map with keys and values and validation error", func() {
+			Expect(result).To(Equal(map[string]string{
+				"this-is-key":         "this-is-value",
+				"this-is-another-key": "this-is-another-value",
+				"key":                 "with=multiple=equalsigns",
+			}))
+			Expect(err).Should(HaveOccurred())
+		})
+	})
+
+	When("flag with successful validation option", func() {
+		BeforeEach(func() {
+			cliArgs = []string{
+				"--flag1", "this-is-key=this-is-value", "this-is-another-key=this-is-another-value", "key=with=multiple=equalsigns",
+				"--other-flag", "another-flag-key=123"}
+			flag = "flag1"
+			opts = []args.ValuesValidateOption{
+				func(values []string) error {
+					return nil
+				},
+			}
+		})
+		It("should return map with keys and values and validation error", func() {
+			Expect(result).To(Equal(map[string]string{
+				"this-is-key":         "this-is-value",
+				"this-is-another-key": "this-is-another-value",
+				"key":                 "with=multiple=equalsigns",
+			}))
+			Expect(err).ShouldNot(HaveOccurred())
+		})
+	})
+
 })
+
+func Test_ValuesValidationOptDuplicatedKeys(t *testing.T) {
+	tests := []struct {
+		name   string
+		pairs  []string
+		errMsg string
+	}{
+		{
+			name:   "nil pairs",
+			pairs:  nil,
+			errMsg: "",
+		},
+		{
+			name:   "empty pairs",
+			pairs:  []string{},
+			errMsg: "",
+		},
+		{
+			name:   "duplicated pairs",
+			pairs:  []string{"foo=1", "foo=2", "foo=3"},
+			errMsg: "invalid env-flags: duplicated env keys: [foo]",
+		},
+		{
+			name:   "invalid pairs",
+			pairs:  []string{"foo", "foo", "bar"},
+			errMsg: "invalid env-flags: invalid env items: [foo bar]",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := args.ValuesValidationOptDuplicatedKeys(tt.pairs)
+			if (err != nil) != (tt.errMsg != "") {
+				t.Errorf("ValuesValidationOptDuplicatedKeys() error = %v, want errmsg: %v", err, tt.errMsg)
+			}
+			if err != nil && err.Error() != tt.errMsg {
+				t.Errorf("ValuesValidationOptDuplicatedKeys() error.Error = %v, want errmsg: %v", err.Error(), tt.errMsg)
+			}
+		})
+	}
+}
