0586fefd088604adf724c56572c5ede78f76e721
nanjingfm <765390765@qq.com>
2023-01-05 12:07:42 +0800

    chore: add helper function to get userinfo from request context

--- user/userinfo_test.go
+++ user/userinfo_test.go
@@ -22,6 +22,9 @@ import (
 	"testing"
 
 	"github.com/emicklei/go-restful/v3"
+	metav1alpha1 "github.com/katanomi/pkg/apis/meta/v1alpha1"
+	. "github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
 )
 
 func TestGetUserInfo(t *testing.T) {
@@ -57,3 +60,42 @@ func TestGetUserInfo(t *testing.T) {
 	})
 
 }
+
+var _ = Describe("Test GetBaseUserInfoFromReq", func() {
+	var (
+		tokenString string
+		authorInfo  *metav1alpha1.GitUserBaseInfo
+	)
+	BeforeEach(func() {
+		tokenString = ""
+		authorInfo = nil
+	})
+	JustBeforeEach(func() {
+		_req := &http.Request{
+			Header: map[string][]string{},
+		}
+		_req.Header.Set("Authorization", tokenString)
+		req := &restful.Request{Request: _req}
+
+		res := &restful.Response{}
+		target := func(req *restful.Request, resp *restful.Response) {}
+		chain := &restful.FilterChain{Target: target}
+		UserInfoFilter(req, res, chain)
+
+		authorInfo = GetBaseUserInfoFromReq(req)
+	})
+
+	Context("when the request context contains the user info", func() {
+		BeforeEach(func() {
+			tokenString = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImVtYWlsIjoiam9obkB0ZXN0LmNvbSIsImlhdCI6MTY1MzM3NzM0MCwiZXhwIjoxNjUzMzgwOTQwfQ.tT5YWrFu2F_0NWg3JFbpIaC-HbaBgJtRcff_O3Ti225RZztON1gpZOfFf06EIzWolkUrtAfrTFXonHx2rh5w8mK82kFX5sLPxiV3yDfvxU9KuE66IW_48ykFU6puBcnVMZNWUtPLHcH4OAq51zbca9eh3AFiFldnJ3YRJ85Bigky8nc1uf3CCm5c9d7P8q5SGDJZvGLHOXqQ4_aSfpX0HDTHbgw_82d7FpckeDYRdFN89LORaLRChbBM1IdfH4JvI9WtO3PDU7Ce49nMmmidhsESAalxfMrN3LIPmMz7vY0FJaBW24oA0FwJvq1Q6O_jzupMHRGS-clkUImRw185cA"
+		})
+		It("should get the username as excepted", func() {
+			gomega.Expect(authorInfo.Name).To(gomega.Equal("John Doe"))
+		})
+	})
+	Context("when the request context dose not contains the user info", func() {
+		It("should get the username as excepted", func() {
+			gomega.Expect(authorInfo).To(gomega.BeNil())
+		})
+	})
+})
