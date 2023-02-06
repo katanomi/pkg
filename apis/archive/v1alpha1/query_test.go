9b1a8fd53758778a0d14def5a21401545149c044
nanjingfm <765390765@qq.com>
2023-02-06 18:02:14 +0800

    feat: add archive related structs

---
+++ apis/archive/v1alpha1/query_test.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2023 The Katanomi Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	ktesting "github.com/katanomi/pkg/testing"
+	"github.com/onsi/gomega"
+)
+
+func checkCondition(file string, t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	cond := Condition{}
+	ktesting.MustLoadYaml(file, &cond)
+	data, _ := json.Marshal(cond)
+	got := map[string]interface{}{}
+	_ = json.Unmarshal(data, &got)
+
+	want := map[string]interface{}{}
+	ktesting.MustLoadYaml(file, &want)
+
+	g.Expect(cmp.Diff(got, want)).To(gomega.BeEmpty())
+}
+
+func TestCondition(t *testing.T) {
+	checkCondition("testdata/condition.or.golden.yaml", t)
+	checkCondition("testdata/condition.and.golden.yaml", t)
+	checkCondition("testdata/condition.in.string.golden.yaml", t)
+}
---
+++ apis/meta/v1alpha1/pager.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2023 The Katanomi Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+// Pager describe the paging params
+type Pager struct {
+	// ItemsPerPage desired number of items to be returned in each page
+	ItemsPerPage int `json:"itemsPerPage"`
+
+	// Page desired to be returned
+	Page int `json:"page"`
+}
+
+// GetPageLimit get the limit returned by a single page
+func (p *Pager) GetPageLimit() int {
+	if p.ItemsPerPage == 0 {
+		p.ItemsPerPage = 20
+	}
+	return p.ItemsPerPage
+}
+
+// GetOffset get the offset for next query
+func (p *Pager) GetOffset() int {
+	return p.GetPageLimit() * p.GetPage()
+}
+
+// GetPage get the current page
+func (p *Pager) GetPage() int {
+	if p.Page == 0 {
+		p.Page = 1
+	}
+	return p.Page
+}
