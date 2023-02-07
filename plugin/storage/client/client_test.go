6c50db96828b8fb270d9280608c0ee2443772b28
jzli <jzli@alauda.io>
2023-02-08 01:10:07 +0800

    chore: add client unittest

--- plugin/storage/client/client_test.go
+++ plugin/storage/client/client_test.go
@@ -19,11 +19,13 @@ package client
 import (
 	"context"
 	goerrors "errors"
+	"net/http"
 	"testing"
 
 	"github.com/go-resty/resty/v2"
 	"github.com/jarcoal/httpmock"
 	pclient "github.com/katanomi/pkg/plugin/client"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -36,32 +38,123 @@ type Body struct {
 	Code    int    `json:"code"`
 }
 
-func TestStoragePluginClientGet(t *testing.T) {
-	g := NewGomegaWithT(t)
-	httpmock.Reset()
-
-	responder, _ := httpmock.NewJsonResponder(200, Body{Message: "Your message", Code: 200})
-
-	fakeUrl := "https://example.com/api/core/v1alpha1/projects"
-	httpmock.RegisterResponder("GET", fakeUrl, responder)
-
-	RESTClient := resty.New()
-	httpmock.ActivateNonDefault(RESTClient.GetClient())
-	url, _ := apis.ParseURL("https://example.com/api/")
-	client := NewStoragePluginClient(&v1.Addressable{
-		URL: url,
-	}, WithGroupVersion(&schema.GroupVersion{
-		Group:   "core",
-		Version: "v1alpha1",
-	}), WithRestClient(RESTClient))
-	result := &Body{}
-	err := client.Get(context.Background(), "projects",
-		pclient.ResultOpts(result),
+var _ = Describe("Test.StoragePluginClient", func() {
+	var (
+		restClient     *resty.Client
+		storageClient  *StoragePluginClient
+		opts           []BuildOptions
+		err            error
+		result         *Body
+		fakeUrl        string
+		baseUrl        string
+		responder      httpmock.Responder
+		mockHttpMethod string
 	)
-	g.Expect(err).To(BeNil())
-	g.Expect(result.Message).To(Equal("Your message"))
-	g.Expect(result.Code).To(Equal(200))
-}
+
+	BeforeEach(func() {
+		responder, _ = httpmock.NewJsonResponder(200, Body{Message: "Your message", Code: 200})
+		restClient = resty.New()
+		opts = make([]BuildOptions, 0)
+		mockHttpMethod = http.MethodGet
+		result = &Body{}
+	})
+
+	JustBeforeEach(func() {
+		httpmock.Reset()
+		httpmock.RegisterResponder(mockHttpMethod, fakeUrl, responder)
+		httpmock.ActivateNonDefault(restClient.GetClient())
+		url, _ := apis.ParseURL(baseUrl)
+
+		opts = append(opts, WithRestClient(restClient))
+		storageClient = NewStoragePluginClient(&v1.Addressable{
+			URL: url,
+		}, opts...)
+	})
+
+	Context("Client HTTP methods", func() {
+		BeforeEach(func() {
+			opts = []BuildOptions{WithGroupVersion(&schema.GroupVersion{
+				Group:   "core",
+				Version: "v1alpha1",
+			})}
+			baseUrl = "https://example.com/api"
+			fakeUrl = "https://example.com/api/core/v1alpha1/projects"
+		})
+
+		JustAfterEach(func() {
+			Expect(err).To(BeNil())
+			Expect(result.Message).To(Equal("Your message"))
+			Expect(result.Code).To(Equal(200))
+		})
+
+		Context("get method", func() {
+			BeforeEach(func() {
+				mockHttpMethod = http.MethodGet
+			})
+
+			It("get correct url and return expected result", func() {
+				err = storageClient.Get(context.Background(), "projects",
+					pclient.ResultOpts(result),
+				)
+			})
+		})
+
+		Context("post method", func() {
+			BeforeEach(func() {
+				mockHttpMethod = http.MethodPost
+			})
+
+			It("post correct url and return expected result", func() {
+				err = storageClient.Post(context.Background(), "projects",
+					pclient.ResultOpts(result),
+				)
+			})
+		})
+
+		Context("put method", func() {
+			BeforeEach(func() {
+				mockHttpMethod = http.MethodPut
+			})
+			It("put correct url and return expected result", func() {
+				err = storageClient.Put(context.Background(), "projects",
+					pclient.ResultOpts(result),
+				)
+			})
+		})
+
+		Context("delete method", func() {
+			BeforeEach(func() {
+				mockHttpMethod = http.MethodDelete
+			})
+			It("delete correct url and return expected result", func() {
+				err = storageClient.Delete(context.Background(), "projects",
+					pclient.ResultOpts(result),
+				)
+			})
+		})
+	})
+
+	Context("clone client with group version", func() {
+		BeforeEach(func() {
+			baseUrl = "https://example.com/api"
+			fakeUrl = "https://example.com/api/foo/v1/bar"
+		})
+		JustAfterEach(func() {
+			Expect(err).To(BeNil())
+			Expect(result.Message).To(Equal("Your message"))
+			Expect(result.Code).To(Equal(200))
+		})
+
+		It("return new client with group version", func() {
+			storageClient = storageClient.ForGroupVersion(&schema.GroupVersion{
+				Group:   "foo",
+				Version: "v1",
+			})
+			err = storageClient.Get(context.Background(), "bar", pclient.ResultOpts(result))
+
+		})
+	})
+})
 
 func TestStoragePluginClientError(t *testing.T) {
 	g := NewGomegaWithT(t)
---
+++ plugin/storage/route/route_suite_test.go
@@ -0,0 +1,93 @@
+/*
+Copyright 2023 The Katanomi Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package route
+
+import (
+	"context"
+	"io"
+	"testing"
+
+	alpha1 "github.com/katanomi/pkg/apis/archive/v1alpha1"
+	metav1alpha1 "github.com/katanomi/pkg/apis/meta/v1alpha1"
+	"github.com/katanomi/pkg/apis/storage/v1alpha1"
+	filestorev1alpha1 "github.com/katanomi/pkg/plugin/storage/capabilities/filestore/v1alpha1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"go.uber.org/zap"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type fakeFileStorePlugin struct{}
+type fakeCorePlugin struct{}
+
+func (f *fakeCorePlugin) Path() string {
+	return "fake-core"
+}
+
+func (f *fakeCorePlugin) Setup(ctx context.Context, logger *zap.SugaredLogger) error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (f *fakeCorePlugin) CheckAuth(ctx context.Context, params []metav1alpha1.Param) (*v1alpha1.StorageAuthCheck, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (f *fakeFileStorePlugin) Aggregate(ctx context.Context, aggs alpha1.AggregateQuery, opts *alpha1.ListOptions) (*alpha1.AggregateResult, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (f *fakeFileStorePlugin) GetFileObject(ctx context.Context, key string) (*filestorev1alpha1.FileObject, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (f *fakeFileStorePlugin) PutFileObject(ctx context.Context, fileReadCloser io.ReadCloser, meta v1alpha1.FileMeta) (*v1alpha1.FileMeta, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (f *fakeFileStorePlugin) DeleteFileObject(ctx context.Context, key string) error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (f *fakeFileStorePlugin) ListFileMetas(ctx context.Context, opt *metav1.ListOptions) ([]v1alpha1.FileMeta, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (f *fakeFileStorePlugin) GetFileMeta(ctx context.Context, key string) (*v1alpha1.FileMeta, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (f *fakeFileStorePlugin) Path() string {
+	return "fake-filestore"
+}
+
+func (f *fakeFileStorePlugin) Setup(ctx context.Context, logger *zap.SugaredLogger) error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func TestRoute(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Route Suite")
+}
