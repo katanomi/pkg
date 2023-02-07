b3c463c92089330f76646cd137b86c6af56d567f
jzli <jzli@alauda.io>
2023-02-08 01:10:07 +0800

    chore: add client unittest

--- plugin/storage/route/route_test.go
+++ plugin/storage/route/route_test.go
@@ -15,3 +15,30 @@ limitations under the License.
 */
 
 package route
+
+import (
+	"github.com/emicklei/go-restful/v3"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Test.Storage.Route.Service", func() {
+	var (
+		svcs []*restful.WebService
+	)
+	Context("NewService with file store plugin", func() {
+		It("returns services with file store routes", func() {
+			svcs, _ = NewServices(&fakeFileStorePlugin{})
+			Expect(svcs).To(HaveLen(1))
+			Expect(svcs[0].RootPath()).To(Equal("/storage/fake-filestore/file-store/v1alpha1"))
+		})
+	})
+
+	Context("NewService with auth plugin", func() {
+		It("returns services with core routes", func() {
+			svcs, _ = NewServices(&fakeCorePlugin{})
+			Expect(svcs).To(HaveLen(1))
+			Expect(svcs[0].RootPath()).To(Equal("/storage/fake-core/core/v1alpha1"))
+		})
+	})
+})
