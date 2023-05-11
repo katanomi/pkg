fea1dffeffa401e23bae58ab320f22a96aca765b
yuzhipeng <yuzp1996@qq.com>
2023-05-11 18:06:51 +0800

    Define some special error for k8s status error (#437)
    For example FileNotFound GitRevisionNotFound etc.
    
    Signed-off-by: yuzhipeng <zpyu@alauda.io>

--- errors/convert_response_error.go
+++ errors/convert_response_error.go
@@ -24,8 +24,8 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-// ConvertResponseError converts a gitlab response and an error into a kubernetes api error
-// ctx is the basic context, response is the response object from gitlab sdk, err is the returned error
+// ConvertResponseError converts a http response and an error into a kubernetes api error
+// ctx is the basic context, response is the response object from tool sdk, err is the returned error
 // gvk is the GroupVersionKind object with type meta for the object
 // names supports one optional name to be given and will be attributed as the resource name in the returned error
 func ConvertResponseError(ctx context.Context, response *http.Response, err error, gvk schema.GroupVersionKind, names ...string) error {
