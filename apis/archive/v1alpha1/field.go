259eb98bb9c8927dcaf200ac6466dc2d2da3eaf2
Kinso <jzli@alauda.io>
2023-07-31 15:40:50 +0800

    fix: add topOwner fields (#469)

--- apis/archive/v1alpha1/field.go
+++ apis/archive/v1alpha1/field.go
@@ -19,30 +19,39 @@ package v1alpha1
 const (
 	// IDField the name of id field, usually the primary key id of the storage table
 	IDField = "id"
+	// TopOwnerClusterField the name of top owner cluster field
+	TopOwnerClusterField = "topOwnerCluster"
 	// TopClusterField the name of top cluster field
 	TopClusterField = "topCluster"
 	// ParentClusterField the name of parent cluster field
 	ParentClusterField = "parentCluster"
 	// ClusterField the name of cluster field
 	ClusterField = "cluster"
+	// TopOwnerUIDField the name of top owner uid field
+	TopOwnerUIDField = "topOwnerUid"
 	// TopUIDField the name of top uid field
 	TopUIDField = "topUid"
 	// ParentUIDField the name of parent uid field
 	ParentUIDField = "parentUid"
 	// UIDField the name of uid field
 	UIDField = "uid"
+	// TopOwnerNamespaceField the name of top owner namespace field
+	TopOwnerNamespaceField = "topOwnerNamespace"
 	// TopNamespaceField the name of top namespace field
 	TopNamespaceField = "topNamespace"
 	// ParentNamespaceField the name of parent namespace field
 	ParentNamespaceField = "parentNamespace"
 	// NamespaceField the name of namespace field
 	NamespaceField = "namespace"
+	// TopOwnerNameField the name of top owner name field
+	TopOwnerNameField = "topOwnerName"
 	// TopNameField the name of top name field
 	TopNameField = "topName"
 	// ParentNameField the name of parent name field
 	ParentNameField = "parentName"
 	// NameField the name of name field
 	NameField = "name"
+
 	// GroupField the name of group field
 	GroupField = "group"
 	// VersionField the name of version field
