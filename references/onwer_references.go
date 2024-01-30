1c4b863cee419e0afccee1dcbcb454a0f56ba8f6
yuzhipeng <yuzp1996@qq.com>
2024-01-30 13:10:35 +0800

    Add FindOwnerReferenceWithGroupKind function for find (#525)
    owner with provided group and kind

--- references/onwer_references.go
+++ references/onwer_references.go
@@ -18,6 +18,8 @@ limitations under the License.
 package references
 
 import (
+	"strings"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
@@ -27,6 +29,27 @@ func FindOwnerReference(owners []metav1.OwnerReference, gvk schema.GroupVersionK
 	for _, owner := range owners {
 		if owner.Kind == gvk.Kind &&
 			owner.APIVersion == gvk.GroupVersion().String() {
+			return &owner
+		}
+	}
+	return nil
+}
+
+// FindOwnerReferenceWithGroupKind find the owner in the owner references with group kind
+func FindOwnerReferenceWithGroupKind(owners []metav1.OwnerReference, gk schema.GroupKind) *metav1.OwnerReference {
+	for _, owner := range owners {
+		if owner.Kind != gk.Kind {
+			continue
+		}
+		// For the legacy v1 resource which apiVersion is v1 without group
+		// https://github.com/kubernetes/kubernetes/blob/5310e4f30e212a3d58b37fd07633c3b249627b53/pkg/apis/core/register.go#L26-L29
+		// https://github.com/kubernetes/kubernetes/blob/5310e4f30e212a3d58b37fd07633c3b249627b53/pkg/apis/core/register.go#L55-L99
+		// https://kubernetes.io/zh-cn/docs/reference/using-api/#api-versioning
+		if owner.APIVersion == "v1" && gk.Group == "" {
+			return &owner
+		}
+
+		if strings.HasPrefix(owner.APIVersion, gk.Group) {
 			return &owner
 		}
 	}
