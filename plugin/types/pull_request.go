6a3fc54fde2251f143a8dec1b26d03043c606b92
nanjingfm <mingfu@alauda.io>
2024-02-04 10:33:58 +0800

    chore: split plugin interface (#528)

--- plugin/types/pull_request.go
+++ plugin/types/pull_request.go
@@ -47,11 +47,29 @@ type GitPullRequestCommentLister interface {
 // GitPullRequestHandler list, get and create pr function
 type GitPullRequestHandler interface {
 	Interface
+	GitPullRequestLister
+	GitPullRequestGetter
+	GitPullRequestCreator
+}
+
+// GitPullRequestLister list pull requests
+type GitPullRequestLister interface {
+	Interface
 	ListGitPullRequest(
 		ctx context.Context,
 		option metav1alpha1.GitPullRequestListOption,
 		listOption metav1alpha1.ListOptions,
 	) (metav1alpha1.GitPullRequestList, error)
+}
+
+// GitPullRequestGetter get a pull request
+type GitPullRequestGetter interface {
+	Interface
 	GetGitPullRequest(ctx context.Context, option metav1alpha1.GitPullRequestOption) (metav1alpha1.GitPullRequest, error)
+}
+
+// GitPullRequestCreator create a new pull request
+type GitPullRequestCreator interface {
+	Interface
 	CreatePullRequest(ctx context.Context, payload metav1alpha1.CreatePullRequestPayload) (metav1alpha1.GitPullRequest, error)
 }
