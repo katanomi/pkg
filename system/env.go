3bb5798b79089f7acea3b28670cafd4960673a26
kychen <kychen@alauda.io>
2024-03-06 15:30:19 +0800

    feat: add base domain env key.

--- system/env.go
+++ system/env.go
@@ -25,10 +25,27 @@ const (
 	// ClusterNameEnvKey is the environment variable that specifies the system cluster name.
 	// ClusterName can be attached to the created resource. When there are multiple clusters, the resource can be found by the api by the cluster name.
 	ClusterNameEnvKey = "SYSTEM_CLUSTER_NAME" // NOSONAR // ignore: "Key" detected here, make sure this is not a hard-coded credential
+
+	// BASE_DOMAIN used to get the data of BASE_DAMAIN from the environment variable.
+	// Usually used as the base value of label or annotation. For example: {BASE_DOMAIN}/label: value.
+	BaseDomainEnvKey = "BASE_DOMAIN"
 )
 
 // ClusterName returns the cluster name that describes the cluster name the instance runs on.
 // You need to specify the relevant environment variables through deployment when deploying.
 func ClusterName() string {
 	return os.Getenv(ClusterNameEnvKey)
+}
+
+// BaseDomain return BASE_DOMAIN value from environment variable. when BASE_DOMAIN not set, will return "katanomi.dev"
+func BaseDomain() string {
+	return fromEnvStr(BaseDomainEnvKey, "katanomi.dev")
+}
+
+func fromEnvStr(key string, defaultStr string) string {
+	str := os.Getenv(key)
+	if str == "" {
+		return defaultStr
+	}
+	return str
 }
