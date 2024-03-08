8f8317cdc0f475909f236375e4b0d5e04425e195
jtcheng <jtcheng@alauda.io>
2024-03-08 11:40:00 +0800

    Add LoadKubeResources in TestContext
    1. when we load kube resource in TestContext, we usually want to creat object namespace according TestContext,
       but we cannot assume the namespace in resource file
    
    Signed-off-by: jtcheng <jtcheng@alauda.io>

--- testing/framework/cluster/kubernetes_test.go
+++ testing/framework/cluster/kubernetes_test.go
@@ -17,10 +17,15 @@ limitations under the License.
 package cluster
 
 import (
-	"github.com/katanomi/pkg/testing"
+	"testing"
+
+	"github.com/docker/distribution/context"
+	ktesting "github.com/katanomi/pkg/testing"
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
@@ -51,7 +56,7 @@ var _ = Describe("ExpectKubeObject", func() {
 					Namespace: "default",
 					Name:      "config-1",
 				},
-			}).Should(testing.DiffEqualTo(&corev1.ConfigMap{
+			}).Should(ktesting.DiffEqualTo(&corev1.ConfigMap{
 				ObjectMeta: metav1.ObjectMeta{
 					Namespace: "default",
 					Name:      "config-1",
@@ -71,10 +76,10 @@ var _ = Describe("ExpectKubeObject", func() {
 					Namespace: "default",
 					Name:      "config-1",
 				},
-			}, testing.KubeObjectDiffClean, func(object interface{}) interface{} {
+			}, ktesting.KubeObjectDiffClean, func(object interface{}) interface{} {
 				delete(object.(*corev1.ConfigMap).Data, "a")
 				return object
-			}).Should(testing.DiffEqualTo(&corev1.ConfigMap{
+			}).Should(ktesting.DiffEqualTo(&corev1.ConfigMap{
 				ObjectMeta: metav1.ObjectMeta{
 					Namespace: "default",
 					Name:      "config-1",
@@ -86,3 +91,80 @@ var _ = Describe("ExpectKubeObject", func() {
 		})
 	})
 })
+
+var _ = Describe("LoadKubeResources", func() {
+	var (
+		clt      ctrlclient.Client
+		file     string
+		converts []ktesting.ConvertRuntimeObjctToClientObjectFunc
+		err      error
+	)
+
+	BeforeEach(func() {
+		clt = fakeclient.NewClientBuilder().WithScheme(clientgoscheme.Scheme).Build()
+		converts = []ktesting.ConvertRuntimeObjctToClientObjectFunc{}
+	})
+	JustBeforeEach(func() {
+		err = LoadKubeResources(&TestContext{Namespace: "default-e2e", Client: clt}, file, converts...)
+	})
+
+	When("file is a kubernetes object that not set namespace", func() {
+		BeforeEach(func() {
+			file = "./testdata/LoadKubeResources.configmap.yaml"
+		})
+		It("should create the object in TestContext namespace", func() {
+			Expect(err).Should(BeNil())
+
+			configMap := &corev1.ConfigMap{}
+			err = clt.Get(context.Background(), ctrlclient.ObjectKey{Namespace: "default-e2e", Name: "default"}, configMap)
+			Expect(err).Should(BeNil())
+			Expect(configMap.Data["a"]).Should(BeEquivalentTo("1"))
+		})
+	})
+
+	When("file is a kubernetes object that set namespace", func() {
+		BeforeEach(func() {
+			file = "./testdata/LoadKubeResources.configmap-withns.yaml"
+		})
+		It("should create the object in namespace the specified in file", func() {
+			Expect(err).Should(BeNil())
+
+			configMap := &corev1.ConfigMap{}
+			err = clt.Get(context.Background(), ctrlclient.ObjectKey{Namespace: "default", Name: "default"}, configMap)
+			Expect(err).Should(BeNil())
+			Expect(configMap.Data["a"]).Should(BeEquivalentTo("1"))
+		})
+	})
+
+	When("use converts", func() {
+		BeforeEach(func() {
+			file = "./testdata/LoadKubeResources.configmap.yaml"
+			converts = append(converts, func(object runtime.Object) (ctrlclient.Object, error) {
+				configmap := object.(*corev1.ConfigMap)
+				configmap.Namespace = "default-e2e"
+				configmap.Annotations = map[string]string{
+					"a": "1",
+				}
+				return configmap, nil
+			})
+		})
+		It("should create the object according change by convertFunc", func() {
+			Expect(err).Should(BeNil())
+
+			configMap := &corev1.ConfigMap{}
+			err = clt.Get(context.Background(), ctrlclient.ObjectKey{Namespace: "default-e2e", Name: "default"}, configMap)
+			Expect(err).Should(BeNil())
+			Expect(configMap.Data["a"]).Should(BeEquivalentTo("1"))
+			Expect(configMap.Annotations["a"]).Should(BeEquivalentTo("1"))
+		})
+	})
+})
+
+func TestLoadKubeResources(t *testing.T) {
+	clt := fakeclient.NewClientBuilder().WithScheme(clientgoscheme.Scheme).Build()
+
+	g := NewGomegaWithT(t)
+	g.Expect(func() {
+		MustLoadKubeResources(&TestContext{Namespace: "default", Client: clt}, "./testdata/LoadKubeResources_invalid.yaml")
+	}).Should(Panic())
+}
