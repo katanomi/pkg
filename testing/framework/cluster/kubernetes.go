19039ed62b266f4feea52fa2d93ffdd281a21b72
chengjingtao <jtcheng0616@gmail.com>
2024-03-08 13:46:40 +0800

    Add LoadKubeResources in TestContext (#552)
    1. when we load kube resource in TestContext, we usually want to creat object namespace according TestContext,
       but we cannot assume the namespace in resource file
    
    Signed-off-by: jtcheng <jtcheng@alauda.io>
    Co-authored-by: jtcheng <jtcheng@alauda.io>

--- testing/framework/cluster/kubernetes.go
+++ testing/framework/cluster/kubernetes.go
@@ -19,6 +19,8 @@ package cluster
 import (
 	"fmt"
 
+	"k8s.io/apimachinery/pkg/runtime"
+
 	"github.com/katanomi/pkg/testing"
 	. "github.com/onsi/gomega"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
@@ -54,7 +56,6 @@ func ExpectKubeObject(ctx *TestContext, object ctrlclient.Object, cleanFuncs ...
 
 	Expect(err).Should(BeNil())
 	object.GetObjectKind().SetGroupVersionKind(gvk)
-	fmt.Printf("===> %#v", object)
 	if len(cleanFuncs) == 0 {
 		cleanFuncs = []func(object interface{}) interface{}{
 			testing.KubeObjectDiffClean,
@@ -65,4 +66,55 @@ func ExpectKubeObject(ctx *TestContext, object ctrlclient.Object, cleanFuncs ...
 		object = clean(object).(ctrlclient.Object)
 	}
 	return Expect(object)
+}
+
+// convertToSetNamespace will set object namespace according TestContext
+func convertToSetNamespace(namespace string) testing.ConvertRuntimeObjctToClientObjectFunc {
+	return func(object runtime.Object) (ctrlclient.Object, error) {
+		obj, ok := object.(ctrlclient.Object)
+		if !ok {
+			err := fmt.Errorf("Unsupported gvk: %s", object.GetObjectKind().GroupVersionKind())
+			return nil, err
+		}
+		if obj.GetNamespace() == "" {
+			obj.SetNamespace(namespace)
+		}
+		return obj, nil
+	}
+}
+
+// LoadKubeResources will load kubernetes resources from file
+// it will use client in ctx and set namespace as ctx.Namespace if namespace is empty in file
+// if you set namespace in file, it will just use it and ignore ctx.Namespace
+// you can set converts to change some extra fields before create in kubernetes
+// but you must pay attention to that when you set more than one converts: just one converts will be executed when match
+// if you set converts , you should set namespace to ctx.Namespace explicitly
+// eg.
+//
+//	LoadKubeResources(ctx, file)// load resource from file and apply to kubernetes with ctx.Namespace and ctx.Client
+//
+//	// load resource from file and execute convert if resource type is configmap before apply to kubernetes with ctx.Namespace and ctx.Client
+//
+//	LoadKubeResources(ctx, file,func(object runtime.Object) (ctrlclient.Object, error) {
+//		configmap, ok := object.(*corev1.ConfigMap)
+//		if !ok {
+//			return nil, fmt.Errorf("Unsupported gvk: %s", object.GetObjectKind().GroupVersionKind())
+//		}
+//		configmap.Namespace = "default-e2e"
+//		configmap.Annotations = map[string]string{
+//			"a": "1",
+//		}
+//		return configmap, nil
+//	})
+func LoadKubeResources(ctx *TestContext, file string, converts ...testing.ConvertRuntimeObjctToClientObjectFunc) (err error) {
+	converts = append(converts, convertToSetNamespace(ctx.Namespace))
+	return testing.LoadKubeResources(file, ctx.Client, converts...)
+}
+
+// MustLoadKubeResources similar with LoadKubeResources but panic if LoadKubeResources error
+func MustLoadKubeResources(ctx *TestContext, file string, converts ...testing.ConvertRuntimeObjctToClientObjectFunc) {
+	err := LoadKubeResources(ctx, file, converts...)
+	if err != nil {
+		panic(fmt.Sprintf("load yaml file failed, file path: %s, err: %s", file, err))
+	}
 }
