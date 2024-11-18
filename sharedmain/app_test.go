98deb8b735c878d00c62a0f1eeb47e3318730e4d
jtcheng <jtcheng@alauda.io>
2024-11-18 18:12:20 +0800

    rename license headers
    Signed-off-by: jtcheng <jtcheng@alauda.io>

--- sharedmain/app_test.go
+++ sharedmain/app_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The Katanomi Authors.
+Copyright 2021 The AlaudaDevops Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ package sharedmain
 import (
 	"testing"
 
-	"github.com/katanomi/pkg/fieldindexer"
+	"github.com/AlaudaDevops/pkg/fieldindexer"
 
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
